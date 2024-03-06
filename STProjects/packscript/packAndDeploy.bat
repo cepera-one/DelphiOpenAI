1caef5f29860d787f9eb7276b9401d5ae95d0bb2
cepera-one <cepera@sprutcam.com>
2024-03-06 17:34:07 +0300

    Few renames in delpoy scripts

--- STProjects/packscript/packAndDeploy.bat
+++ STProjects/packscript/packAndDeploy.bat
@@ -5,11 +5,11 @@ SET StartDir=%~dp0..
 
 call :RUNCOPY AIHelperD.dll
 
-nuget pack package\SprutTechnology.AIHelperD.nuspec
+nuget pack package\SprutCAMTech.AIHelperD.nuspec
 
 SET source=https://nexus.office.sprut.ru/repository/dev-feed/index.json
 SET api=<enter api key here>
-REM nuget push SprutTechnology.AIHelperD.1.0.0.nupkg -Source %source% -apiKey %api%
+nuget push SprutCAMTech.AIHelperD.1.0.0.nupkg -Source %source% -apiKey %api%
 
 pause
 
